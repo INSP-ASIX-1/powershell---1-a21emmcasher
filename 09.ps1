ba625930952449d7814ef99820db314e5682baf2
a21emmcasher <95307567+a21emmcasher@users.noreply.github.com>
2022-02-11 16:06:21 +0100

    ex 9

--- 09.ps1
+++ 09.ps1
@@ -1,1 +1,9 @@
 #si dos numeros passats per parametre a l'script son iguals mostra OK. Si son diferents mostra KO
+$valor1 = Read-Host "Primer valor"
+$valor2 = Read-Host "Segon valor"
+if($value1 -eq $value2){
+    write-Output "OK"
+}
+else{
+    write-Output "KO"
+}
